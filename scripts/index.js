e5785476c5370b3d1a3670df585269c050efae46
Pranto408 <prantodatta408@gmail.com>
2026-03-09 01:01:36 +0600

    Update Sign In Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
 const userName = document.getElementById("username");
 const password = document.getElementById("password");
 const signInBtn = document.getElementById("sign-in-btn");
+
 //Sign In Authentication
 signInBtn.addEventListener("click", () => {
-    if (userName.value === "admin" && password.value === "admin123") {
-        alert("Sign In Successful");
-        window.location.href = "./home.html";
+  if (userName.value === "admin" && password.value === "admin123") {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid Username or Password! Please try again");
+  }
+});
+
+// Enter key Sign In
+[userName, password].forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      signInBtn.click();
     }
-    else {
-        alert("Invalid Username or Password! Please try again");
-    }
-})
+  });
+});
